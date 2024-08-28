e0084772a399b54dffb485db39ce7363c235054c
Tanner Hebel <thebel117@gmail.com>
2024-08-27 22:24:26 -0400

    various small debugs -TH

--- PhiloWellnessMVC.Services/obj/Debug/net8.0/PhiloWellnessMVC.Services.AssemblyInfo.cs
+++ PhiloWellnessMVC.Services/obj/Debug/net8.0/PhiloWellnessMVC.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhiloWellnessMVC.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3773df129a28aaea49ae6783813f1867a0c328c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14013ce1cf1398e1f60d8eac867868fd8792fec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhiloWellnessMVC.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhiloWellnessMVC.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
